d54f59aabd3435db871f698e8c4b0023eeb7dd91
izuknown <77488180+izuknown@users.noreply.github.com>
2023-11-08 13:55:09 +0000

    Update audio_transcription.py

--- audio_transcription.py
+++ audio_transcription.py
@@ -1,3 +1,18 @@
 import streamlit as st
+import whisper
 
-st.write("Hello World")
+st.title("Transcription Service")
+
+#upload audio file with streamlit
+audio_file = st.file_uploader("Upload Audio", type=["wav", "mp3", "m4a"])
+
+
+@st.cache
+def load_whipser_model ():
+    model = whisper.load_model("base") #there are various types of models available, each with differenct RAM requirments 
+    return model
+
+if st.sidebar.button("Load Whisper Model"):
+    model = load_whipser_model()
+    st.sidebar.success("Whisper Model Loaded")
+
